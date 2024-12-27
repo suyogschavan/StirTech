c295b35802be01523789d1464e22ae25c72812b8
Suyog <suyogschavan03@gmail.com>
2024-12-27 12:04:48 +0530

    bug2

--- models/Record.js
+++ models/Record.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const record = new mongoose.Schema({
-    trend1: {type:String, required:true},
-    trend2: {type:String, required:true},
-    trend3: {type:String, required:true},
-    trend4: {type:String, required:true},
-    trend5: {type:String, required:true},
+    trend1: {type:String, required:false},
+    trend2: {type:String, required:false},
+    trend3: {type:String, required:false},
+    trend4: {type:String, required:false},
+    trend5: {type:String, required:false},
     timestamp: {type:Date, default:Date.now},
 });
 
