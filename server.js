b76e75aac97fe7c5c92c0c0406eb935d51817211
Suyog <suyogschavan03@gmail.com>
2024-12-27 18:22:56 +0530

    bug fixing

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ async function runScraper(io) {
 
     async function scrollToBottom(driver) {
         await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        await driver.sleep(2000);  
+        await driver.sleep(1000);  
     }
 
     const options = new chrome.Options();
@@ -89,18 +89,18 @@ async function runScraper(io) {
         // Step 1: Log in to X (formerly Twitter)
         await io.emit('log', 'Navigating to login page...');
         await driver.get('https://x.com/login');
-        await sleep(3000);
+        // await sleep(3000);
         await io.emit('log', 'On login page ');
 
         // Enter username
-        const usernameField = await driver.wait(until.elementLocated(By.xpath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input")), 10000);
+        const usernameField = await driver.wait(until.elementLocated(By.name("text")), 10000);
         await usernameField.sendKeys(process.env.TWITTER_USERNAME, '\n');
         await io.emit('log', 'Entered username');
 
         // Wait for password field
-        await sleep(3000);
+        // await sleep(3000);
 
-        const passwordField = await driver.wait(until.elementLocated(By.xpath("/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input")), 10000);
+        const passwordField = await driver.wait(until.elementLocated(By.name("password")), 10000);
         await passwordField.sendKeys(process.env.TWITTER_PASSWORD, '\n');
         await io.emit('log', 'Entered password');
 
